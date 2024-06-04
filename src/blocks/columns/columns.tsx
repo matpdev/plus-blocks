949f5ecf9108578deb21baf91e9cd1ffde7a8e0e
Matheus Alves <matheus2ep@gmail.com>
2024-06-04 13:36:17 -0300

    fix(columns): fix columns props error

--- src/blocks/columns/columns.tsx
+++ src/blocks/columns/columns.tsx
@@ -3,9 +3,30 @@
 import classNames from 'classnames';
 import React from 'react';
 
-export default function Columns(
-  { children }: React.PropsWithChildren,
-  { classname, wrap = false }: { classname?: string; wrap?: boolean },
-) {
-  return <div className={classNames('flex', wrap ? 'flex-wrap' : '', classname, 'md-mw:flex-wrap')}>{children}</div>;
+export default function Columns({
+  children,
+  classname,
+  wrap,
+  layout,
+}: React.PropsWithChildren & {
+  classname?: string;
+  wrap?: boolean;
+  layout?: {
+    type?: string;
+    justifyContent?: string;
+  };
+}) {
+  return (
+    <div
+      className={classNames(
+        !!layout?.type ? layout.type : 'flex',
+        wrap ? 'flex-wrap' : '',
+        !!layout?.justifyContent ? 'justify-' + layout.justifyContent : '',
+        classname,
+        'md-mw:flex-wrap',
+      )}
+    >
+      {children}
+    </div>
+  );
 }
